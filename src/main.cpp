1b50ef3ca6085141667d52cd74013390bfc28b1d
Gregor Vollmer <git@dynamic-noise.net>
2016-08-17 15:21:17 +0200

    Very basic analysis software skeletton

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+
+#include <regex>
+#include <iostream>
+#include <getopt.h>
+
+void print_help(char* argv[])
+{
+	std::cout << "" << std::endl;
+}
+
+void print_usage(char* argv[])
+{
+	std::cout << argv[0] << ": [-r runlist.csv] MAPSA_RUN" << std::endl;
+}
+
+void print_version(char* argv[])
+{
+}
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
