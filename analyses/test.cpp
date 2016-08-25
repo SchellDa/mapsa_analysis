7181d4c799e6dbba253c70380006bc6c727805d8
Gregor Vollmer <git@dynamic-noise.net>
2016-08-25 17:08:14 +0200

    Test analysis now displays a list of defined variables plus their values
    Maybe useful for debugging cfg file issues?

--- analyses/test.cpp
+++ analyses/test.cpp
@@ -1,6 +1,7 @@
 
 #include "test.h"
 #include <iostream>
+#include <algorithm>
 
 REGISTER_ANALYSIS_TYPE(Test, "A dummy analysis for testing the framework.")
 
@@ -17,7 +18,18 @@ Test::~Test()
 
 void Test::run(const po::variables_map& vm)
 {
-	std::cout << "Running analysis" << std::endl;
+	std::cout << "The following configuration variables where defined at runtime:\n";
+	auto variables = _config.getDefinedVariables();
+	std::sort(variables.begin(), variables.end());
+	for(const auto& var: variables) {
+		try {
+			auto val = _config.getVariable(var);
+			std::cout << " " << var << " = " << val << "\n";
+		} catch(std::exception& e) {
+			std::cout << "ERROR! " << var << " : " << e.what() << "\n";
+		}
+	}
+	std::cout << std::flush;
 }
 
 std::string Test::getHelp(const std::string& argv0) const
