510600098fb0baaad928201639f68ff1bbdd15f8
Gregor Vollmer <git@dynamic-noise.net>
2016-08-26 14:09:04 +0200

    Documented AbstractFactory and Analysis classes

--- core/include/abstractfactory.h
+++ core/include/abstractfactory.h
@@ -9,11 +9,28 @@
 
 namespace core {
 
+/** \brief Generic factory pattern class
+ *
+ * This class can be used to dynamicaly generate objects identified by their type name. All objects must be
+ * derived from a common base class T. The AbstractFactory is a singleton itself, with one object per class
+ * type T and description_T type.
+ *
+ * The objects are created by a functor object returning an std::shared_ptr<T> that takes no arguments.
+ * If the objects can be created by the default constructor, one can use the
+ * #REGISTER_FACTORY_TYPE(T, type) and #REGISTER_FACTORY_TYPE_WITH_DESCR(T, type, descr) macros. They
+ * invoke RegisterCreator() on the AbstractFactory<T> and pass a functor calling std::make_shared<T>.
+ *
+ * \tparam T Abstract object interface class, base-type for generated objects
+ * \tparam description_T Type of the describing meta-information. std::string by default.
+ */
 template<typename T, typename description_T=std::string>
 class AbstractFactory
 {
 public:
+	/// A functor returning an object of type T
 	typedef std::function<std::shared_ptr<T>()> creator_t;
+
+	/// Get the AbstractFactory singleton object
 	static AbstractFactory<T>* Instance()
 	{
 		if(s_instance == nullptr) {
@@ -27,11 +44,24 @@ public:
 	{
 	}
 
+	/** \brief Register creator function
+	 *
+	 * A type registration by a unique name and a creator functor returning a new object.
+	 * \param type Identifier of the new type. Should be the C++ class name.
+	 * \param fct Functor returning a new object with base-type T.
+	 */
 	bool RegisterCreator(const std::string& type, const creator_t& fct)
 	{
 		return RegisterCreator(type, fct, description_T{});
 	}
-
+	/** \brief Register creator function with description object
+	 *
+	 * A type registration by a unique name and a creator functor returning a new object. An object
+	 * describing the type can be passed.
+	 * \param type Identifier of the new type. Should be the C++ class name.
+	 * \param fct Functor returning a new object with base-type T.
+	 * \param descr Informative description of the new type.
+	 */
 	bool RegisterCreator(const std::string& type, const creator_t& fct, const description_T& descr)
 	{
 		_creators[type] = fct;
@@ -39,6 +69,8 @@ public:
 		return true;
 	}
 
+	/** \brief Return a vector with all registered type names
+	 */
 	std::vector<std::string> getTypes() const
 	{
 		std::vector<std::string> a;
@@ -48,11 +80,24 @@ public:
 		return a;
 	}
 
+	/** \brief Get description object for a specified type
+	 *
+	 * \param type Identifier of the type to get description about
+	 * \throw std::out_of_range The requested type was not registered.
+	 */
 	description_T getDescription(const std::string& type) const
 	{
 		return _descriptions.at(type);
 	}
 
+	/** \brief Create new object of specified type
+	 *
+	 * Calls the functor object passed via RegisterCreator() and returns
+	 * the created object.
+	 * \param type Type-identifier of the object to create
+	 * \return Shared pointer to new object of specified type
+	 * \throw std::out_of_range The requested type was not registered.
+	 */
 	std::shared_ptr<T> create(const std::string& type) const
 	{
 		return _creators.at(type)();
@@ -67,8 +112,26 @@ private:
 template<typename T, typename description_T>
 AbstractFactory<T, description_T>* AbstractFactory<T, description_T>::s_instance;
 
+/** \brief Register new type to specific factory
+ *
+ * Adds a new type to the AbstractFactory<base> by invoking RegisterCreator() and passing a functor to
+ * std::make_shared<type>. The type name is the C++ identifier passed as type.
+ * \param type C++ class identifier of new type class
+ * \param base Base class identifier to identify the correct factory
+ */
 #define REGISTER_FACTORY_TYPE(base, type) static bool BOOST_PP_CAT(type, __regged) = core::AbstractFactory<base>::Instance()->RegisterCreator(BOOST_PP_STRINGIZE(type), std::bind(&std::make_shared<type>));
 
+/** \brief Register new type to specific factory
+ *
+ * Adds a new type to the AbstractFactory<base> by invoking RegisterCreator() and passing a functor to
+ * std::make_shared<type>. The type name is the C++ identifier passed as type.
+ *
+ * Additionaly, a descriptive object descr is passed.
+ *
+ * \param type C++ class identifier of new type class
+ * \param base Base class identifier to identify the correct factory
+ * \param descr Descriptive object of the type specified for the AbstractFactory
+ */
 #define REGISTER_FACTORY_TYPE_WITH_DESCR(base, type, descr) static bool BOOST_PP_CAT(type, __regged) = core::AbstractFactory<base>::Instance()->RegisterCreator(BOOST_PP_STRINGIZE(type), std::bind(&std::make_shared<type>), descr);
 
 }//namespace core
--- core/include/analysis.h
+++ core/include/analysis.h
@@ -10,18 +10,70 @@ namespace po = boost::program_options;
 
 namespace core {
 
+/** \brief Abstract analysis interface
+ *
+ * Sub-classes of Analysis should implement the run() method to perform analysis tasks. The sub-classes should
+ * register themselves with #REGISTER_ANALYSIS_TYPE(type, description) to the factory class. An executable
+ * can then select which analysis object to create and run.
+ *
+ * On construction several command line options are registered. The sub-class may add additional options using
+ * the boost::program_options::option_description object returned by getOptionsDescription().
+ *
+ * The loadConfig() implementation of Analysis attempts to load a configuration file if passed with the -c
+ * command line option and applies any additional variables from the -D command line option.
+ *
+ * The functions getUsage() and getHelp() display default help messages if the -h option is specified or an
+ * error in the command line arguments are found. A sub-class should reimplement them if additional options
+ * are implemented. Note that getHelp() only displays a free-text help message and not a option-by-option
+ * documentation. Each option is autodocumented by Boost.
+ */
 class Analysis
 {
 public:
 	Analysis();
 	virtual ~Analysis() {}
+	/** \brief Load configuration from file and from command line
+	 *
+	 * Loads configuration file specified by -c option and executes any string given by -D as additional
+	 * configuration text.
+	 * \throw std::ios_base::failure Configuration file cannot be found
+	 * \throw core::CfgParse::parse_error Configuration by file or command line cannot be parsed
+	 */
 	virtual void loadConfig(const po::variables_map& vm);
+	/** \brief Perform analysis. Must be reimplemented.
+	 *
+	 * This is the work-horse to perform any actual analysis.
+	 */
 	virtual void run(const po::variables_map& vm) = 0;
 
-	po::options_description& getOptionsDescriptionRef() { return _options; }
-	po::options_description getOptionsDescription() const { return _options; }
+	/** \brief Get boost::program_options::options_description object to add further command line
+	 * arguments
+	 */
+	const po::options_description& getOptionsDescription() const { return _options; }
+	po::options_description& getOptionsDescription() { return _options; }
 
+	/** \brief Get string describing the tool usage from the command line
+	 *
+	 * Sub-classes should reimplement this to return useful information. The default implementation merely
+	 * advices to run -h for further help.
+	 *
+	 * The usage string usualy displays a command line with the different possible short-hand arguments,
+	 * e.g. \verbatim ./analysis MyAnalysis [-h] [-c cfgfile] [-Dvar=val ...] [-o outfile] [--verbose] \endverbatim
+	 *
+	 * \return Usage string
+	 * \param argv0 The command to call the analysis. Usually something like
+	 * \verbatim ./command AnalysisType \endverbatim
+	 */
 	virtual std::string getUsage(const std::string& argv0) const;
+	/** \brief Return help message to display before the option-by-option documentation
+	 *
+	 * \param argv0 The command to call the analysis. Usually something like
+	 * \verbatim ./command AnalysisType \endverbatim
+	 * Can be used for displaying example invokations.
+	 * \return A free-text help message describing what the analysis does. Displayed before the
+	 * option-by-option documentation when showing the help text via -h. Default implementation returns
+	 * an empty string.
+	 */
 	virtual std::string getHelp(const std::string& argv0) const;
 
 protected:
@@ -31,7 +83,16 @@ private:
 	po::options_description _options;
 };
 
+/** \brief short-hand type for the factory class for core::Analysis. */
 typedef AbstractFactory<Analysis, std::string> AnalysisFactory;
+
+/** \brief Register new analysis type
+ *
+ * Register an new analysis type. It must be a subclass of core::Analysis. Call this makro from the implementation
+ * file of your analysis class.
+ * \param type C++ identifier of the analysis to register.
+ * \param descr String with a short description of the analysis.
+ */ 
 #define REGISTER_ANALYSIS_TYPE(type, descr) REGISTER_FACTORY_TYPE_WITH_DESCR(core::Analysis, type, descr)
 
 }// namespace core
