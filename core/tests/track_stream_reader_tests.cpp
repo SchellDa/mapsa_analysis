7ca475a8e95d04264a358c44edc07d27d564dab2
Gregor Vollmer <git@dynamic-noise.net>
2016-08-26 11:47:48 +0200

    Bugfix: TrackStreamReader did not correctly recognize lines with negative numbers
    An exception was thrown when a data line contained a negative number,
    the regular expression was fixed and two new test cases were added.

--- core/tests/track_stream_reader_tests.cpp
+++ core/tests/track_stream_reader_tests.cpp
@@ -13,6 +13,37 @@ public:
 	{
 		char s[4096];
 		std::ofstream fout;
+		float_wo_decimal = std::tmpnam(s);
+		fout.open(s);
+		fout << "# X     Y       Z       SensorID        Evt     Run\n"
+		     << "1.0000\t0.0000\t0\t0\t11\t4\n"
+		     << "0.0000\t1.0000\t0\t1\t11\t4\n"
+		     << "0.0000\t0.0000\t3\t2\t11\t4\n"
+		     << "6.0000\t5.0000\t4\t3\t11\t4\n\n\n"
+		     << "1.0000\t0.0000\t0\t0\t15\t4\n"
+		     << "0.0000\t2.0000\t0\t1\t15\t4\n"
+		     << "0.0000\t0.0000\t3\t2\t15\t4\n"
+		     << "6.0000\t5.0000\t4\t3\t15\t4\n\n\n"
+		     << "1.0000\t0.0000\t0\t0\t15\t4\n"
+		     << "0.0000\t2.0000\t0\t1\t15\t4\n"
+		     << "0.0000\t0.0000\t3\t2\t15\t4\n"
+		     << "6.0000\t5.0000\t4\t3\t15\t4\n\n\n"
+		     << "1.0000\t0.0000\t0\t0\t54\t4\n"
+		     << "0.0000\t2.0000\t0\t1\t54\t4\n"
+		     << "0.0000\t0.0000\t3\t2\t54\t4\n"
+		     << "6.0000\t5.0000\t4\t3\t54\t4\n\n\n"
+		     << "1.0000\t0.0000\t0\t0\t75\t4\n"
+		     << "0.0000\t2.0000\t0\t1\t75\t4\n"
+		     << "0.0000\t0.0000\t3\t2\t75\t4\n"
+		     << "6.0000\t5.0000\t4\t3\t75\t4\n\n\n"
+		     << "1.0000\t0.0000\t0\t0\t99\t4\n"
+		     << "0.0000\t2.0000\t0\t1\t99\t4\n"
+		     << "0.0000\t0.0000\t3\t2\t99\t4\n"
+		     << "6.0000\t5.0000\t4\t3\t99\t4";
+		fout.flush();
+		fout.close();
+
+
 		bad_evt_order = std::tmpnam(s);
 		fout.open(s);
 		fout << "# X     Y       Z       SensorID        Evt     Run\n"
@@ -135,6 +166,26 @@ public:
 		     << "6.0000\t5.0000\t4.0000\t3\t99\t4\n\n";
 		fout.flush();
 		fout.close();
+
+
+		negatives = std::tmpnam(s);
+		fout.open(s);
+		fout << "# X     Y       Z       SensorID        Evt     Run\n"
+		     << "-1.0000\t0.0000\t0.0000\t0\t11\t4\n"
+		     << "0.0000\t1.0000\t0.0000\t1\t11\t4\n"
+		     << "0.0000\t-0.0000\t3.0000\t2\t11\t4\n"
+		     << "6.0000\t5.0000\t4.0000\t3\t11\t4\n\n\n"
+		     << "1.0000\t0.0000\t0.0000\t0\t15\t4\n"
+		     << "0.0000\t-2.0000\t0.0000\t1\t15\t4\n"
+		     << "0.0000\t0.0000\t3.0000\t2\t15\t4\n"
+		     << "6.0000\t5.0000\t4.0000\t3\t15\t4\n\n\n"
+		     << "-1.0000\t0.0000\t0.0000\t0\t15\t4\n"
+		     << "0.0000\t2.0000\t0.0000\t1\t15\t4 # Comment!!\n"
+		     << "0.0000\t0.0000\t3.0000\t2\t15\t4\n"
+		     << "# Yes, quite commentry\n"
+		     << "6.0000\t5.0000\t4.0000\t3\t15\t4\n\n\n";
+		fout.flush();
+		fout.close();
 	}
 
 	virtual void TearDown()
@@ -143,8 +194,12 @@ public:
 		std::remove(valid2.c_str());
 		std::remove(valid3.c_str());
 		std::remove(bad_evt_order.c_str());
+		std::remove(float_wo_decimal.c_str());
+		std::remove(negatives.c_str());
 	}
 
+	std::string negatives;
+	std::string float_wo_decimal;
 	std::string bad_evt_order;
 	std::string parse_error;
 	std::string valid1;
@@ -209,6 +264,41 @@ TEST(trackstreamreader, read2)
 TEST(trackstreamreader, read3)
 {
 	TrackStreamReader reader(env->valid3);
+	int totalEvts = 0;
+	int eventNumbers[] = {11, 15, 54, 75, 99};
+	size_t numTracks[] = {1, 2, 1, 1, 1};
+	for(const auto& evt: reader) {
+		EXPECT_EQ(evt.eventNumber, eventNumbers[totalEvts]) << "Wrong event number";
+		EXPECT_EQ(evt.tracks.size(), numTracks[totalEvts]) << "Wrong number of tracks in event "
+			<< evt.eventNumber;
+		EXPECT_EQ(evt.runID, 4);
+		for(const auto& tr: evt.tracks) {
+			EXPECT_EQ(tr.sensorIDs.size(), 4);
+			EXPECT_EQ(tr.points.size(), 4);
+			EXPECT_NO_THROW({
+				EXPECT_EQ(tr.sensorIDs.at(0), 0);
+				EXPECT_EQ(tr.sensorIDs.at(1), 1);
+				EXPECT_EQ(tr.sensorIDs.at(2), 2);
+				EXPECT_EQ(tr.sensorIDs.at(3), 3);
+			});
+		}
+		totalEvts += 1;
+	}
+	EXPECT_EQ(totalEvts, 5);
+}
+
+TEST(trackstreamreader, negative_numbers)
+{
+	EXPECT_NO_THROW({
+		TrackStreamReader reader(env->negatives);
+		for(const auto& evt: reader) {
+		}
+	});
+}
+
+TEST(trackstreamreader, float_wo_decimal)
+{
+	TrackStreamReader reader(env->float_wo_decimal);
 	int totalEvts = 0;
 	int eventNumbers[] = {11, 15, 54, 75, 99};
 	size_t numTracks[] = {1, 2, 1, 1, 1};
