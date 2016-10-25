99f88ec891d72078df5fe1f59500bd3b23e0f915
Gregor Vollmer <git@dynamic-noise.net>
2016-10-25 18:38:40 +0200

    StripAlign: implemented XZ-alignment and Y-alignment processors
    Both alignment-steps are run sequentialy in different processors and
    can be skipped if required via command line.

--- analyses/strip_align.cpp
+++ analyses/strip_align.cpp
@@ -11,10 +11,17 @@ StripAlign::StripAlign() :
  Analysis(), _file(nullptr)
 {
 	getOptionsDescription().add_options()
+		("low-y", po::value<double>()->default_value(-100), "Lower bound of Y align scan")
+		("high-y", po::value<double>()->default_value(100), "Upper bound of Y align scan")
 		("low-z", po::value<double>()->default_value(820), "Lower bound of Z align scan")
 		("high-z", po::value<double>()->default_value(860), "Upper bound of Z align scan")
+		("reuse-z", "If set, use an already calculated Z alignment and only redo the Y alignment.")
+		("no-y", "Do not calculate Y alignment.")
 		("num-steps,s", po::value<int>()->default_value(10), "Number of steps in the range (low,high)")
 		("sample-size,n", po::value<int>()->default_value(10000), "Number of data points to include in alignment histogram")
+		("low-shift", po::value<int>()->default_value(-10), "Lower shift value for --shift mode")
+		("high-shift", po::value<int>()->default_value(10), "Upper shift value for --shift mode")
+		("shift", "Do not scan different Z positions but different data shift values.")
 	;
 	addProcess("scan", /* CS_ALWAYS */ CS_TRACK,
 	           core::Analysis::init_callback_t {},
@@ -23,10 +30,18 @@ StripAlign::StripAlign() :
 	           core::Analysis::run_post_callback_t {},
 	           std::bind(&StripAlign::scanFinish, this)
 	           );
+	addProcess("yAlign", /* CS_ALWAYS */ CS_TRACK,
+	           core::Analysis::init_callback_t {},
+	           std::bind(&StripAlign::yAlignInit, this),
+		   std::bind(&StripAlign::yAlignRun, this, std::placeholders::_1, std::placeholders::_2),
+	           core::Analysis::run_post_callback_t {},
+	           std::bind(&StripAlign::yAlignFinish, this)
+	           );
 }
 
 StripAlign::~StripAlign()
 {
+	_out.close();
 	if(_file) {
 		_file->Write();
 		_file->Close();
@@ -36,33 +51,90 @@ StripAlign::~StripAlign()
 
 void StripAlign::init(const po::variables_map& vm)
 {
-	_out.open(getFilename(".csv"));
+	_out.open(getFilename(".csv"), std::ofstream::out);
+	if(!_out.good()) {
+		throw std::ios_base::failure("Cannot open debug output file.");
+	}
 	_file = new TFile(getFilename(".root").c_str(), "RECREATE");
 	assert(_file);
 	_sampleSize = vm["sample-size"].as<int>();
+	_lowY = vm["low-y"].as<double>();
+	_highY = vm["high-y"].as<double>();
 	_lowZ = vm["low-z"].as<double>();
 	_highZ = vm["high-z"].as<double>();
 	_numSteps = vm["num-steps"].as<int>();
-	_currentScanStep = 0;
-	int num_plots = _numSteps + 2;
-	int n_x = std::sqrt(num_plots);
-	int n_y = std::sqrt(num_plots);
-	while(n_x*n_y < num_plots) {
-		++n_y;
+	_lowShift = vm["low-shift"].as<int>();
+	_highShift = vm["high-shift"].as<int>();
+	_shift = vm.count("shift") > 0;
+	_reuseZ	= vm.count("reuse-z") > 0;
+	_noY = vm.count("no-y") > 0;
+	if(_shift) {
+		assert(_highShift > _lowShift);
+		_numSteps = _highShift - _lowShift + 1;
 	}
-	int width = n_x * 400;
-	int height = n_y * 300;
-	assert(n_x > 0);
-	assert(n_y > 0);
-	assert(n_x*n_y >= num_plots);
-	_canvas = new TCanvas("Canvas", "Canvas", width, height);
-	_canvas->Divide(n_x, n_y);
+	_currentScanStep = 0;
+	{
+		int num_plots = _numSteps + 2;
+		int n_x = std::sqrt(num_plots);
+		int n_y = std::sqrt(num_plots);
+		while(n_x*n_y < num_plots) {
+			++n_y;
+		}
+		int width = n_x * 400;
+		int height = n_y * 300;
+		assert(n_x > 0);
+		assert(n_y > 0);
+		assert(n_x*n_y >= num_plots);
+		_canvas = new TCanvas("Canvas", "Canvas", width, height);
+		_canvas->Divide(n_x, n_y);
+	}
+	{
+		int num_plots = _numSteps;
+		int n_x = std::sqrt(num_plots);
+		int n_y = std::sqrt(num_plots);
+		while(n_x*n_y < num_plots) {
+			++n_y;
+		}
+		int width = n_x * 400;
+		int height = n_y * 300;
+		assert(n_x > 0);
+		assert(n_y > 0);
+		assert(n_x*n_y >= num_plots);
+		_canvasX = new TCanvas("CanvasX", "Canvas", width, height);
+		_canvasY = new TCanvas("CanvasY", "Canvas", width, height);
+		_canvasX->Divide(n_x, n_y);
+		_canvasY->Divide(n_x, n_y);
+	}
 	_canvas->cd(1);
-	auto txt = new TText(.5, .5, "Hallo Welt!");
+	auto txt = new TText(.5, .5, "");
 	txt->SetTextAlign(22);
 	txt->SetTextSize(0.2);
 	txt->Draw();
-	std::remove(getFilename(".csv").c_str());
+	if(_reuseZ) {
+		std::ifstream alignfile(getFilename("_best.align"));
+		if(alignfile.good()) {
+			double x, y, z, sigma;
+			alignfile >> x >> y >> z >> sigma;
+			_zAlignment.position = Eigen::Vector3d(x, y, z);
+			_zAlignment.sigma = sigma;
+			if(!alignfile.good()) {
+				std::cout << "Error while reading alignment file, recalculate Z alignment." << std::endl;
+				_reuseZ = false;
+			}
+			std::cout << "Loaded the following XZ-Alignment:"
+			          << "\n  Position "
+				  << _zAlignment.position(0) << "mm "
+				  << _zAlignment.position(1) << "mm "
+				  << _zAlignment.position(2) << "mm"
+				  << "\n  Sigma " << _zAlignment.sigma << "mm" << std::endl;
+		} else {
+			std::cout << "No alignment file found, recalculate Z alignment." << std::endl;
+			_reuseZ = false;
+		}
+	}
+	if(_reuseZ && _noY) {
+		throw std::runtime_error("Reuse Z alignment and do not calculate Y alignment? Great, do nothing... asshole!");
+	}
 }
 
 std::string StripAlign::getUsage(const std::string& argv0) const
@@ -77,20 +149,44 @@ std::string StripAlign::getHelp(const std::string& argv0) const
 
 void StripAlign::scanInit()
 {
+	if(_reuseZ) {
+		return;
+	}
 	assert(_file);
 	_numProcessedSamples = 0;
-	_currentZ = _lowZ + (_highZ - _lowZ) / (_numSteps-1) * _currentScanStep;
-	_config.setVariable("mpa_z_offset", _currentZ);
-	std::cout << _currentScanStep << "/" << _numSteps
-	          << ": Calculate alignment constants for Z=" << _currentZ << std::endl;
-	_corHist = new TH1D((std::string("align_")+std::to_string(_currentScanStep)).c_str(), "align", 2000, -200, 200);
+	if(_shift) {
+		_currentShift = _lowShift + _currentScanStep;
+		setDataOffset(_currentShift);
+		_currentZ = _lowZ;
+		_config.setVariable("mpa_z_offset", _lowZ);
+	} else {
+		_currentZ = _lowZ + (_highZ - _lowZ) / (_numSteps-1) * _currentScanStep;
+		_config.setVariable("mpa_z_offset", _currentZ);
+	}
+	std::cout << _currentScanStep+1 << "/" << _numSteps
+	          << ": Calculate alignment constants for Z = " << _currentZ
+		  << "mm, skip = " << getDataOffset() << std::endl;
+	std::string header = "Z = ";
+	header += std::to_string(_currentZ);
+	header += "mm, skip = ";
+	header += std::to_string(getDataOffset());
+	_corHist = new TH1D((std::string("align_")+std::to_string(_currentScanStep)).c_str(), header.c_str(), 4000, -10, 10);
+	_corX = new TH2D((std::string("cor_x_")+std::to_string(_currentScanStep)).c_str(),
+	                 (std::string("X: ")+header).c_str(), 50, -10, 10, 50, -10, 10);
+	_corY = new TH2D((std::string("cor_y_")+std::to_string(_currentScanStep)).c_str(),
+	                 (std::string("Y: ")+header).c_str(), 50, -10, 10, 50, -10, 10);
 	_file->Add(_corHist);
+	_file->Add(_corX);
+	_file->Add(_corY);
 	_currentSigmaMinimum = { 0.0, -1.0 };
 }
 
 bool StripAlign::scanRun(const core::TrackStreamReader::event_t& track_event,
                       const core::BaseSensorStreamReader::event_t& mpa_event)
 {
+	if(_reuseZ) {
+		return false;
+	}
 	int n_strips = _config.get<int>("strip_count");
 	double pitch = _config.get<double>("strip_pitch");
 	double length = _config.get<double>("strip_length");
@@ -98,33 +194,48 @@ bool StripAlign::scanRun(const core::TrackStreamReader::event_t& track_event,
 		auto b = track.extrapolateOnPlane(1, 3, _currentZ, 2);
 		for(const auto& idx: mpa_event.data) {
 			double x = (static_cast<double>(idx) - n_strips/2) * pitch;
+			_out << mpa_event.eventNumber << "\t"
+			     << b(0) << "\t"
+			     << b(1) << "\t"
+			     << b(2) << "\t"
+			     << idx << "\t"
+			     << x << std::endl;
 			//std::cout << idx << " -> " << x << std::endl;
 			_corHist->Fill(b(0) - x);
+			_corX->Fill(b(0), x);
+			_corY->Fill(b(1), x);
 		}
 	}
+	_out << std::endl;
 	return (_numProcessedSamples++ < _sampleSize);
 }
 
 void StripAlign::scanFinish()
 {
+	if(_reuseZ)
+		return;
+	_canvasX->cd(_currentScanStep+1);
+	_corX->Draw("COLZ");
+	_canvasY->cd(_currentScanStep+1);
+	_corY->Draw("COLZ");
+	_out.flush();
 	int cd = _currentScanStep+2;
 	std::cout << "cd-ing to Pad " << cd << std::endl;
 	_canvas->cd(cd);
-	auto result = _corHist->Fit("gaus", "SAME");
+	double mean = _corHist->GetMean();
+	double rms = _corHist->GetRMS();
+	auto result = _corHist->Fit("gaus", "SAME", "", mean-rms*2, mean+rms*2);
 	double sigma = result->Parameter(2);
+	double x = result->Parameter(1);
 	_alignments.push_back({
 		Eigen::Vector3d(
-			0, 0,
+			x, 0,
 			_currentZ),
 		sigma
 	});
 	if(_currentSigmaMinimum(1) < sigma || _currentSigmaMinimum(1) < 0) {
 		_currentSigmaMinimum = { _currentZ, sigma };
 	}
-	std::string title = "Z = ";
-	title += std::to_string(_currentZ);
-	title += " mm";
-	_corHist->SetTitle(title.c_str());
 	_corHist->Draw();
 	++_currentScanStep;
 	if(_currentScanStep < _numSteps) {
@@ -135,7 +246,11 @@ void StripAlign::scanFinish()
 		xgraph->SetName("x_sigma");
 		for(size_t i=0; i < _alignments.size(); ++i) {
 			const auto& align = _alignments[i];
-			xgraph->SetPoint(i, align.position(2), align.sigma);
+			if(_shift) {
+				xgraph->SetPoint(i, _lowShift+i, align.sigma);
+			} else {
+				xgraph->SetPoint(i, align.position(2), align.sigma);
+			}
 		}
 		_file->Add(xgraph);
 		double x_min = xgraph->GetX()[0];
@@ -149,7 +264,7 @@ void StripAlign::scanFinish()
 			}
 		}
 		_canvas->cd(_currentScanStep+2);
-		xgraph->Draw("ACP");
+		xgraph->Draw("A*");
 		_canvas->Update();
 
 		auto img = TImage::Create();
@@ -157,15 +272,117 @@ void StripAlign::scanFinish()
 		img->WriteImage(getFilename(".png").c_str());
 		delete img;
 
-		std::ofstream of(getFilename("_best.align"));
-		auto align = _alignments[best_idx];
-		of << align.position(0) << " "
-		   << align.position(1) << " "
-		   << align.position(2) << " "
-		   << align.sigma << " "
-		   << 0.0 << "\n";
-		of.flush();
-		of.close();
+		img = TImage::Create();
+		img->FromPad(_canvasX);
+		img->WriteImage(getFilename("_cor_x.png").c_str());
+		delete img;
+
+		img = TImage::Create();
+		img->FromPad(_canvasY);
+		img->WriteImage(getFilename("_cor_y.png").c_str());
+		delete img;
+
+		const auto& align = _alignments[best_idx];
+		_zAlignment = align;
+		std::cout << "Best aligment at Z = " << align.position(2) << "mm with a sigma of "
+		          << align.sigma << "mm" << std::endl;
+		writeAlignment(align);
 	}
 }
 
+void StripAlign::yAlignInit()
+{
+	if(_noY) return;
+	_numProcessedSamples = 0;
+	_currentY = _lowY + (_highY - _lowY)/(_numSteps) * getRerunNumber();
+	_yHitcounter[_currentY] = 0;
+	std::cout << getRerunNumber()+1 << "/" << _numSteps
+	          << ": Count hits for Y = " << _currentY
+		  << "mm" << std::endl;
+}
+
+bool StripAlign::yAlignRun(const core::TrackStreamReader::event_t& track_event,
+                           const core::BaseSensorStreamReader::event_t& mpa_event)
+{
+	if(_noY) return false;
+	const double sensor_active_x = 11.860; // mm, 127 strips + bias ring
+	const double sensor_active_y = 48.522; // mm, strip length
+	double x_low = _zAlignment.position(0) - sensor_active_x/2;
+	double x_high = _zAlignment.position(0) + sensor_active_x/2;
+	double y_low = _currentY - sensor_active_y/2;
+	double y_high = _currentY + sensor_active_y/2;
+	auto& counter = _yHitcounter[_currentY];
+	for(const auto& track: track_event.tracks) {
+		auto b = track.extrapolateOnPlane(1, 3, _zAlignment.position(2), 2);
+		//std::cout << _currentY << " -> "
+		//          << b(0) << " "
+		//          << b(1) << ": ["
+		//	  << x_low << " "
+		//	  << x_high << "] ["
+		//	  << y_low << " "
+		//	  << y_high << "]" << std::endl;
+		// Only count tracks hitting active sensor area
+		if(b(0) > x_low && b(0) < x_high &&
+		   b(1) > y_low && b(1) < y_high) {
+			++counter;
+		}
+	}
+//	std::cout << "counter: " << _yHitcounter[_currentY] << std::endl;
+	return (_numProcessedSamples++ < _sampleSize);
+}
+
+void StripAlign::yAlignFinish()
+{
+	if(_noY) return;
+	if(getRerunNumber() < _numSteps) {
+		rerun();
+	} else {
+		double best_y = _yHitcounter.begin()->first;
+		double best_y_max = best_y;
+		double best_hits = _yHitcounter.begin()->second;
+		auto canvas = new TCanvas("canvas", "");
+		auto graph = new TGraph(_yHitcounter.size());
+		size_t i=0;
+		for(const auto& point: _yHitcounter) {
+			graph->SetPoint(i++, point.first, point.second);
+			if(best_hits < point.second) {
+				best_y = point.first;
+				best_y_max = best_y;
+				best_hits = point.second;
+			} else if(best_hits == point.second) {
+				best_y_max = point.first;
+			}
+		}
+		graph->SetTitle("Y alignment");
+		graph->GetXaxis()->SetTitle("Y position (mm)");
+		graph->GetYaxis()->SetTitle("# track hits");
+		graph->Draw("A*");
+		auto img = TImage::Create();
+		img->FromPad(canvas);
+		img->WriteImage(getFilename("_yalign.png").c_str());
+		delete img;
+		std::cout << "Maximum number of hits (" << best_hits << ") for Y = "
+		          << best_y << "mm to " << best_y_max << "mm\n";
+		_zAlignment.position(1) = (best_y + best_y_max)/2;
+		std::cout << "Compromise on mean value: Y = " << _zAlignment.position(1) << "mm" << std::endl;
+		writeAlignment(_zAlignment);
+		std::cout << "Final alignment:"
+		          << "\n  Position "
+			  << _zAlignment.position(0) << "mm "
+			  << _zAlignment.position(1) << "mm "
+			  << _zAlignment.position(2) << "mm"
+			  << "\n  Sigma " << _zAlignment.sigma << "mm" << std::endl;
+	}
+}
+
+void StripAlign::writeAlignment(const alignment_t& align) const
+{
+	std::ofstream of(getFilename("_best.align"));
+	of << align.position(0) << " "
+	   << align.position(1) << " "
+	   << align.position(2) << " "
+	   << align.sigma << " "
+	   << 0.0 << "\n";
+	of.flush();
+	of.close();
+}
--- analyses/strip_align.h
+++ analyses/strip_align.h
@@ -5,6 +5,7 @@
 #include "analysis.h"
 #include "aligner.h"
 #include <TH1D.h>
+#include <TH2D.h>
 #include <TCanvas.h>
 #include <TFile.h>
 #include <fstream>
@@ -20,27 +21,49 @@ public:
 	virtual std::string getHelp(const std::string& argv0) const;
 
 private:
-	void scanInit();
-        bool scanRun(const core::TrackStreamReader::event_t& track_event,
-	             const core::BaseSensorStreamReader::event_t& mpa_event);
-	void scanFinish();
-
 	struct alignment_t {
 		Eigen::Vector3d position;
 		double sigma;
 	};
 
+	void scanInit();
+        bool scanRun(const core::TrackStreamReader::event_t& track_event,
+	             const core::BaseSensorStreamReader::event_t& mpa_event);
+	void scanFinish();
+
+	void yAlignInit();
+        bool yAlignRun(const core::TrackStreamReader::event_t& track_event,
+	               const core::BaseSensorStreamReader::event_t& mpa_event);
+	void yAlignFinish();
+
+	void writeAlignment(const alignment_t& align) const;
+
 	TFile* _file;
 	TCanvas* _canvas;
+	TCanvas* _canvasX;
+	TCanvas* _canvasY;
 	TH1D* _corHist;
+	TH2D* _corX;
+	TH2D* _corY;
 	std::vector<alignment_t> _alignments;
+	alignment_t _zAlignment;
 	double _lowZ;
 	double _highZ;
 	double _currentZ;
+	double _lowY;
+	double _highY;
+	double _currentY;
+	int _lowShift;
+	int _highShift;
+	bool _shift;
+	bool _reuseZ;
+	bool _noY;
+	int _currentShift;
 	int _currentScanStep;
 	int _numSteps;
 	int _numProcessedSamples;
 	int _sampleSize;
+	std::map<double, int> _yHitcounter;
 	std::ofstream _out;
 	Eigen::Vector2d _currentSigmaMinimum;
 };
