444e2f22e93aeceb230db7cb291f15d090b573b0
Gregor Vollmer <git@dynamic-noise.net>
2016-12-13 13:54:20 +0100

    mpatransformimages: Added test files for MPA rotation

--- utils/mpatransformimages.cpp
+++ utils/mpatransformimages.cpp
@@ -4,6 +4,33 @@
 #include <fstream>
 #include <stdio.h>
 
+void angularPoints()
+{
+	core::MpaTransform transform;
+	transform.setOffset({0, 0, 0});
+	std::ofstream of("angular.csv");
+	of << "# Index\tWorld X\tWorld Y\tWorld Z\tTheta\tPhi\tOmega\n";
+	size_t num_frames = 0;
+	double roty = 0.0;
+	double rotz = 0.0;
+	for(double rotx = -1.570796; rotx < 1.570796; rotx += 0.005) {
+		roty += 0.02;
+		rotz += 0.1;
+		transform.setRotation({rotx, roty, rotz});
+		for(double x=0.0; x <= transform.num_pixels_x+0.04; x += 0.25) {
+			for(double y=0.0; y <= transform.num_pixels_y+0.04; y += 0.25) {
+				auto pc = transform.pixelCoordToGlobal(Eigen::Vector2d{x, y});
+				of << pc(0) << "\t" << pc(1) << "\t" << pc(2) << "\t"
+				   << rotx << "\t" << roty << "\t" << rotz << "\n";
+			}
+		}
+		of << "\n\n";
+		++num_frames;
+	}
+	of << "# num_frames = " << num_frames << "\n";
+	of.close();
+}
+
 int main(int argc, char* argv[]) {
 	core::MpaTransform transform;
 	std::ofstream pixel_grid_file("pixel_grid.csv");
@@ -32,6 +59,20 @@ int main(int argc, char* argv[]) {
 		
 	}
 	random_hit_file.close();
+	std::ofstream random_hit_rotated("random_hit_rotated.csv");
+	random_hit_rotated << "# In X\tIn Y\tOut X\tOut Y\n";
+	transform.setRotation({3.141*0.2, 3.141*0.01, 3.141*0.002});
+	for(size_t i = 0; i < 100; ++i) {
+		double x = static_cast<double>(rand())/RAND_MAX * 16;
+		double y = static_cast<double>(rand())/RAND_MAX * 3;
+		auto global = transform.pixelCoordToGlobal(Eigen::Vector2d{x, y});
+		auto back = transform.globalToPixelCoord(global);
+		random_hit_rotated << x << "\t"
+		                << y << "\t"
+				<< back(0) << "\t"
+				<< back(1) << "\n";
+	}
+	random_hit_rotated.close();
 	std::ofstream pixel_index_file("pixel_index_file.csv");
 	pixel_index_file << "# Index\tPixel X\tPixel Y\tWorld X\tWorld Y\n";
 	for(size_t i = 0; i < transform.num_pixels; ++i) {
@@ -42,5 +83,6 @@ int main(int argc, char* argv[]) {
 		                 << wc(0) << "\t" << wc(1) << "\n";
 	}
 	pixel_index_file.close();
+	angularPoints();
 	return 0;
 }
