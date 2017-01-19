ff02d15f755707ccf4d466af3201cc4f6e5fee17
Gregor Vollmer <git@dynamic-noise.net>
2017-01-19 18:45:09 +0100

    Added helper scripts for MpaCmaesAlign multiruns
    They re-execute the alignment and aggregate the
    resulting data into a (not so) compact SQLite3
    database for further processing. Intended
    tool is the visucmaes GUI application.

---
+++ utils/cmaes2sqlite.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+import sqlite3
+
+def scanDirectory(directory):
+    job_files = {}
+    regexFile = re.compile(
+    r"MpaCmaesAlign_num([0-9]+)_([0-9]+)(_path\.csv|_space\.csv|\.status|\.config|\.cache)")
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            m = regexFile.search(f)
+            if m:
+                job_id = int(m.group(1))
+                run_id = int(m.group(2))
+                filetype = m.group(3)
+                if not job_id in job_files:
+                    job_files[job_id] = {
+                        "run_id": run_id,
+                        "job_id": job_id,
+                    }
+                types = ["path", "space", "status", "config", "cache"]
+                for t in types:
+                    if t in filetype:
+                        job_files[job_id][t+"_file"] = os.path.join(root, f)
+    return job_files
+
+def createDatabase(database_file):
+    print "Database: '{}'".format(database_file)
+    if os.path.exists(database_file):
+        print "Database file exists! Exiting."
+        sys.exit(1)
+    conn = sqlite3.connect(database_file)
+    c = conn.cursor()
+    c.execute("""CREATE TABLE `jobs` (job_id INTEGER PRIMARY_KEY, run_id INTEGER,
+            exit_status INTEGER);""")
+    
+    c.execute("""CREATE TABLE config_float (job_id INTEGER, key STRING,
+    value FLOAT,
+    PRIMARY KEY (job_id, key));""")
+    
+    c.execute("""CREATE TABLE config_int (job_id INTEGER, key STRING,
+    value INTEGER,
+    PRIMARY KEY (job_id, key));""")
+    
+    c.execute("""
+CREATE TABLE path (
+ ind INTEGER PRIMARY KEY,
+ job_id INTEGER,
+ generation int,
+ x float,
+ y float,
+ z float,
+ phi float,
+ theta float,
+ omega float,
+ fitness float,
+ sigma float
+);
+""")
+    
+    c.execute("""
+CREATE TABLE parameterspace (
+ id INTEGER PRIMARY KEY,
+ job_id INTEGER,
+ feval_no int,
+ x float,
+ y float,
+ z float,
+ phi float,
+ theta float,
+ omega float,
+ fitness float,
+ total_tracks float,
+ used_tracks float
+);
+""")
+
+    c.execute("""
+CREATE TABLE intermediate_status (
+ id INTEGER PRIMARY KEY,
+ job_id INTEGER,
+ iteration INTEGER,
+ status INTEGER
+);
+""")
+    
+    c.execute("""
+CREATE TABLE hitpairs (
+ id INTEGER PRIMARY KEY,
+ run_id INTEGER,
+ mpa_index INTEGER,
+ ax float,
+ ay float,
+ az float,
+ bx float,
+ by float,
+ bz float
+);
+""")
+    conn.commit();
+    return conn
+
+def createIndex(conn):
+    print "Create indices..."
+    c = conn.cursor()
+    c.execute("CREATE INDEX `index_jobs_run_id` ON `jobs` (run_id);")
+    c.execute("CREATE INDEX `index_jobs_exit_status` ON `jobs` (exit_status);")
+    c.execute("CREATE INDEX index_config_float_value ON config_float(value);")
+    c.execute("CREATE INDEX index_config_int_value ON config_int(value);")
+    c.execute("CREATE INDEX index_path_job_id ON path(job_id);")
+    c.execute("CREATE INDEX index_parameterspace_fitness ON "
+              "parameterspace(fitness);")
+    c.execute("CREATE INDEX index_intermediate_status_job_id ON "
+              "intermediate_status(job_id);")
+    c.execute("CREATE INDEX index_hitpairs_run_id ON hitpairs(run_id);")
+    print "Commit changes..."
+    conn.commit();
+    print "... done!"
+
+def readCsvFile(filename, datatype=float):
+    data = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+            data.append([datatype(x) for x in line.split()])
+    return data
+
+
+def writeJobToDatabase(c, job, write_cache):
+    job_id = job["job_id"]
+    run_id = job["run_id"]
+    status_list = readCsvFile(job["status_file"], int)
+    exit_status = status_list[-1][1]
+    for idx, _ in enumerate(status_list):
+        status_list[idx].append(job_id)
+    c.executemany("""
+INSERT INTO intermediate_status ( iteration, status, job_id ) VALUES(?,?,?)""",
+status_list)
+    c.execute("INSERT INTO jobs VALUES (?, ?, ?)", (job_id, run_id,
+           exit_status))
+    config_list = readCsvFile(job["config_file"], lambda x: x)
+    config_int = []
+    config_float = []
+    for cfg in config_list:
+        if cfg[0] == "int":
+            config_int.append([job_id, cfg[1], int(cfg[2])])
+        else:
+            config_float.append([job_id, cfg[1], float(cfg[2])])
+    c.executemany("INSERT INTO config_int VALUES(?,?,?)", config_int)
+    c.executemany("INSERT INTO config_float VALUES(?,?,?)", config_float)
+    path_list = readCsvFile(job["path_file"])
+    for idx, _ in enumerate(path_list):
+        path_list[idx].append(job_id)
+        path_list[idx].append(idx)
+    c.executemany("""
+INSERT INTO path ( fitness, x, y, z, phi, theta, omega, sigma, job_id, generation ) VALUES (?,?,?,?,?,?,?,?,?,?);
+""", path_list)
+    space_list = readCsvFile(job["space_file"])
+    for idx, _ in enumerate(space_list):
+        space_list[idx].append(job_id)
+        space_list[idx].append(idx)
+    c.executemany("""
+INSERT INTO parameterspace ( fitness, x, y, z, phi, theta, omega, total_tracks,
+used_tracks, job_id, feval_no ) VALUES (?,?,?,?,?,?,?,?,?,?,?);
+""", space_list)
+    if write_cache:
+        cache_list = readCsvFile(job["cache_file"])
+        for idx, _ in enumerate(cache_list):
+            cache_list[idx].append(run_id)
+        c.executemany("""
+    INSERT INTO hitpairs ( mpa_index, ax, ay, az, bx, by, bz, run_id ) VALUES
+    (?,?,?,?,?,?,?,?)
+    """, cache_list)
+
+def main():
+    if len(sys.argv) != 3:
+        print "Usage: {} DATA_PATH OUTPUT_DATABASE".format(sys.argv[0])
+        sys.exit(1)
+    db = createDatabase(sys.argv[2])
+    job_files = scanDirectory(sys.argv[1])
+    job_no = 0
+    for job_no, job in enumerate(job_files.itervalues()):
+        sys.stdout.write("\r\x1b[1KConverting job {} of {}".format(job_no+1,
+            len(job_files)))
+        sys.stdout.flush()
+        run_id = job["run_id"]
+        with db:
+            c = db.cursor()
+            c.execute(
+            "SELECT count(id) FROM hitpairs WHERE run_id = ? LIMIT 1", (run_id,))
+            write_cache = c.fetchone()[0] == 0
+        try:
+            with db:
+                writeJobToDatabase(db, job, write_cache)
+            db.commit()
+        except Exception as e:
+            print "Got error: {}\nIgnoring.".format(str(e))
+    sys.stdout.write("\r\x1b[1KConversion finished!\n")
+    sys.stdout.flush()
+    createIndex(db)
+if __name__ == "__main__":
+    main()
+
---
+++ utils/cmaes_multiscan.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#####################
+## Helper script for multiple alignment job submission.
+## All configuration is passed in environment variables.
+##  RUNS
+##  NUM_RETRIES
+##  LAMBDA_START & LAMBDA_STOP
+##  LAMBDA_STEP
+##  LAMBDAS
+##  NUM_SAMPLES
+##  MPA_UTIL_SCRIPT_PATH
+##  MPA_UTIL_BIN_PATH
+####################
+
+if [ -z ${MPA_UTIL_SCRIPT_PATH} ]; then
+	echo "Please point MPA_UTIL_SCRIPT_PATH to utility script directory. Usually \$SOURCES/utils"
+	exit 1
+fi
+
+if [ -z ${MPA_UTIL_BIN_PATH} ]; then
+	echo "Please point MPA_UTIL_BIN_PATH to utility binaries directory. Usually \$SOURCES/build/utils"
+	exit 1
+fi
+
+# only hard-coded config. Path were MpaCmaesAlign stores data.
+OUTPUT_PATH="/scratch/vollmer/mpaAnalysisOutput"
+
+# Apply default values if env is unset
+if [ -z "${RUNS}" ]; then
+	RUNS="28 63 174"
+	echo "Default RUNS=${RUNS}"
+fi
+if [ -z ${NUM_RETRIES} ]; then
+	NUM_RETRIES=1000
+	echo "Default NUM_RETRIES=${NUM_RETRIES}"
+fi
+if [ -z ${NUM_SAMPLES} ]; then
+	NUM_SAMPLES=30000000
+	echo "Default NUM_SAMPLES=${NUM_SAMPLES}"
+fi
+
+if [ -z "${LAMBDAS}" ]; then
+	if [ -z ${LAMBDA_START} ] || [ -z ${LAMBDA_STOP} ]; then
+		LAMBDA_START=20
+		LAMBDA_STOP=20
+		echo "Default LAMBDA_START=${LAMBDA_START}"
+		echo "    and LAMBDA_STOP=${LAMBDA_STOP}"
+	fi
+	if [ -z ${LAMBDA_STEP} ]; then
+		LAMBDA_STEP=1
+		echo "Default LAMBDA_STEP=${LAMBDA_STEP}"
+	fi
+	LAMBDAS=`seq ${LAMBDA_START} ${LAMBDA_STEP} ${LAMBDA_STOP}`
+	echo "LAMBDAS not set, generating sequence from ${LAMBDA_START} to ${LAMBDA_STOP} in ${LAMBDA_STEP} increments."
+fi
+
+
+CMD_FILE=".cmds"
+MULTI_PREFIX=`date "+%Y-%m-%d__%H-%M-%S"`
+
+DATA_PATH="${OUTPUT_PATH}/MpaCmaesAlign/${MULTI_PREFIX}"
+
+# Generate job file
+rm -f ${CMD_FILE}
+num=1
+for run in ${RUNS}; do
+	echo "Preparing run ${run}..."
+	for lambda in ${LAMBDAS}; do
+		for retry in `seq ${NUM_RETRIES}`; do
+			echo "./analyses/analyses MpaCmaesAlign -C -M ${MULTI_PREFIX} --run ${run} -n ${NUM_SAMPLES} -D \"cmaes_lambda=${lambda}\" -D \"output_prefix=num${num}\" > /dev/null 2>&1" >> ${CMD_FILE}
+			num=`expr ${num} + 1`
+		done
+	done
+done
+
+# Execute jobs
+time ${MPA_UTIL_BIN_PATH}/batchsubmit < ${CMD_FILE}
+pkill analyses
+
+# Merge data in sqlite3 database
+python ${MPA_UTIL_SCRIPT_PATH}/cmaes2sqlite.py ${DATA_PATH} ${OUTPUT_PATH}/multirun_${MULTI_PREFIX}.dat
+
